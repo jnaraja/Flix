609e5c1649a4f2c56132ffa54301c51fc0ef3ac6
Jon-Jon <j2naraja@gmail.com>
2020-09-08 02:21:21 -0700

    Fixed poster sizing

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Jonathan Naraja on 9/8/20.
+//  Copyright © 2020 Codepath. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+}
